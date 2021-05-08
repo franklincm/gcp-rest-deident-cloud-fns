4bae3b209dacfe9195267bf0d9df9bb444643604
Chase Franklin <cfranklin@nuvalence.io>
2021-05-08 17:58:01 -0500

    mock resolve_DLP

--- tests/test_resolve_DLP.py
+++ tests/test_resolve_DLP.py
@@ -1,24 +1,39 @@
 import base64
 import unittest
-from unittest.mock import MagicMock, patch
+import pytest
+from unittest.mock import MagicMock, Mock, patch
 
 from main import resolve_DLP
 
-class TestCreateDLPJob(unittest.TestCase):
 
-    @patch("main.pubsub.PublisherClient")
+class TestCreateDLPJob(unittest.TestCase):
+    def job_result(self):
+        jr = MagicMock()
+        jr.name = MagicMock()
+        jr.name.return_value = "test"
+        jr.inspect_details.result.info_type_stats = [
+            MagicMock(),
+            MagicMock(),
+            MagicMock(),
+        ]
+        jr.inspect_details.requested_options.job_config.storage_config.cloud_storage_options.file_set.url = (
+            "test-file.txt"
+        )
+        return jr
+
+    @patch("main.pubsub.PublisherClient.publish")
     @patch("main.StorageClient")
     @patch("main.DlpServiceClient.get_dlp_job")
     def test_resolve_DLP(self, mocked_dlp, mocked_storage, mocked_publisher):
-        mocked_dlp.get_dlp_job.return_value = MagicMock()
-        mocked_storage.get_bucket.return_value = MagicMock()
+        mocked_dlp.return_value = self.job_result()
+        mocked_storage.return_value = MagicMock()
+        mocked_publisher.return_value = MagicMock()
 
-        data = {
-            "attributes" : {
-                "DlpJobName" : "test"
-            }
-        }
-        
+        data = {"attributes": {"DlpJobName": "test"}}
+
         resolve_DLP(data, None)
-        
-        mocked_dlp.assert_called()
+
+        mocked_publisher.assert_called_with(
+            "projects/gcp-rest-deident/topics/new-sensitive-doc", b"test-file.txt"
+        )
+        mocked_dlp.assert_called_with(request={"name": "test"})
